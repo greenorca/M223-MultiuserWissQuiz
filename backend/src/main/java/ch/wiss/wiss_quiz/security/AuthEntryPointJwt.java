bbc5780f27d8f9609b587270a177ccdfb3fbc8be
Sven Schirmer <sven.glueckspilz@mastersong.de>
2023-02-28 14:51:14 +0100

    remove unused imports

--- backend/src/main/java/ch/wiss/wiss_quiz/security/AuthEntryPointJwt.java
+++ backend/src/main/java/ch/wiss/wiss_quiz/security/AuthEntryPointJwt.java
@@ -2,8 +2,6 @@ package ch.wiss.wiss_quiz.security;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
