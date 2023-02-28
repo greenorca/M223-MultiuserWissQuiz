0b3b84177d6118aa7d6068326b30bfd582bd1a39
Sven Schirmer <sven.glueckspilz@mastersong.de>
2023-02-28 15:51:58 +0100

    Disable CORS and add PublicController for simple home page

--- backend/src/main/java/ch/wiss/wiss_quiz/controller/QuestionController.java
+++ backend/src/main/java/ch/wiss/wiss_quiz/controller/QuestionController.java
@@ -4,13 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import ch.wiss.wiss_quiz.model.Answer;
 import ch.wiss.wiss_quiz.model.AnswerRepository;
@@ -19,7 +20,8 @@ import ch.wiss.wiss_quiz.model.CategoryRepository;
 import ch.wiss.wiss_quiz.model.Question;
 import ch.wiss.wiss_quiz.model.QuestionRepository;
 
-@Controller // This means that this class is a Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController // This means that this class is a Controller
 @RequestMapping(path="/question") // This means URL's start with /question (after Application path)
 public class QuestionController {
   @Autowired
--- backend/src/main/java/ch/wiss/wiss_quiz/controller/QuizController.java
+++ backend/src/main/java/ch/wiss/wiss_quiz/controller/QuizController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import ch.wiss.wiss_quiz.model.CategoryRepository;
 import ch.wiss.wiss_quiz.model.Question;
 import ch.wiss.wiss_quiz.model.QuestionRepository;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/quiz")
 public class QuizController {
