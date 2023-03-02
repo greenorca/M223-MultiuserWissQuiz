4d2feb76b8fd2a9afe119fb4aa6a9663d0379dfb
Sven Schirmer <sven.glueckspilz@mastersong.de>
2023-03-02 14:16:06 +0100

    One does not log out of JWT backend
    removed API call for logout for it doesn't make sense
    see (https://dev.to/_arpy/how-to-log-out-when-using-jwt-4ajm)[https://dev.to/_arpy/how-to-log-out-when-using-jwt-4ajm]

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -6,40 +6,46 @@ const API_URL = "http://localhost:8080/api/auth/";
  * The service uses Axios for HTTP requests 
  * and Local Storage for user information & JWT.
  */
-class AuthService {
+const register = (username, email, password) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    password,
+  });
+};
 
-  login(username, password) {
-    return axios
-      .post(API_URL + "signin", {
-        username,
-        password
-      })
-      .then(response => { 
-        if (response.data.accessToken) { 
-          // this is the insecure part: access token is stored in localStorage
-          // together with username and roles...
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
-
-        return response.data;
-      });
-  }
-
-  logout() {
-    localStorage.removeItem("user");
-  }
-
-  register(username, email, password) {
-    return axios.post(API_URL + "signup", {
+const login = (username, password) => {
+  return axios
+    .post(API_URL + "signin", {
       username,
-      email,
-      password
-    });
-  }
+      password,
+    })
+    .then((response) => {
+      if (response.data.username) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
 
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
-  }
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+  // one cannot simply logout with jwt... see https://dev.to/_arpy/how-to-log-out-when-using-jwt-4ajm
+  /*return axios.post(API_URL + "signout").then((response) => {
+    return response.data;
+  });*/
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
 }
 
-export default new AuthService();
+export default AuthService;
